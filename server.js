76c06ec7d786cd45b041b9568d11349b6b59ba50
Nadege <nadege-externe.rouelle@erdf.fr>
2016-06-24 10:13:31 +0200

    Adding regex to get id from url

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var querystring = require('querystring');
 var server = http.createServer( (req, res) => {
     var page = url.parse(req.url).pathname;
     var params = querystring.parse(url.parse(req.url).query);
+
+    var regexTask = new RegExp("\/task\/[0-9]*");
     
     console.log(page);
     res.writeHead(200, {"Content-Type": "text/html"});
@@ -20,6 +22,18 @@ var server = http.createServer( (req, res) => {
     else if(page =='/etage/1/chambre') {
         res.write('C\'est privé !');
     }
+    else if(regexTask.test(page)) {
+        var regexId = new RegExp("\/task\/([0-9]*)");
+        var id = regexId.exec(page)[1];
+
+        res.writeHead(200, {"Content-Type": "application/json"});
+
+        var sample = {'id':1, 'title':'toto'};
+        var sampleDatas = [{'id':1, 'title':'toto'}, {'id':2, 'title':'tata'}];
+
+        res.write(JSON.stringify(sampleDatas[id]));
+        res.end();
+    }
     else if(page =='/task') {
         res.writeHead(200, {"Content-Type": "application/json"});
 
@@ -28,9 +42,6 @@ var server = http.createServer( (req, res) => {
 
         res.write(JSON.stringify(sampleDatas));
         res.end();
-        // res.write("Id = " + sample.id);
-        // res.write("Titre = " + sample.title);
-        // res.write("Id = " + sampleDatas[1].id);
     }
     else
     {
